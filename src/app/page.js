b3ad1ad03083dc9c6b298695d8b5d58285ba9ab9
Salman Irfan <salmanirfan674@gmail.com>
2023-07-29 20:13:29 +0500

    hard coded blogs

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import Navbar from '../components/Navbar'
 import MainHeading from '../components/MainHeading'
+import Blog from '@/components/Blog';
 
 export default function Home() {
   return (
 		<main>
 			<Navbar />
-			<MainHeading/>
+      <MainHeading />
+      <Blog/>
 		</main>
   );
 }
---
+++ src/components/Blog.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Blog = () => {
+	return (
+		<div>
+			<div className="blogs">
+                <div className="blogItem  text-center py-4">
+                    <h3>How to learn JavaScript</h3>
+                    <p>JavaScript is a language used to develop logic for the web</p>
+                </div>
+                <div className="blogItem  text-center py-4">
+                    <h3>How to learn JavaScript</h3>
+                    <p>JavaScript is a language used to develop logic for the web</p>
+                </div>
+                <div className="blogItem  text-center py-4">
+                    <h3>How to learn JavaScript</h3>
+                    <p>JavaScript is a language used to develop logic for the web</p>
+                </div>
+                <div className="blogItem  text-center py-4">
+                    <h3>How to learn JavaScript</h3>
+                    <p>JavaScript is a language used to develop logic for the web</p>
+                </div>
+			</div>
+		</div>
+	);
+};
+
+export default Blog;
